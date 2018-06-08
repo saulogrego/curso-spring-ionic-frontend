e7a4088d8330386a733bc39bf9804c1a9897fe92
Saulo Grego <saulogrego@gmail.com>
2018-06-08 15:42:24 -0300

    Salvando os dados do usuário logado no localStorage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -29,9 +29,10 @@ export class HomePage {
   }
   
   login(){
-    this.auth.authenticate(this.creds).subscribe(response => {
-      console.log(response.headers.get('Authorization'));
-      this.navCtrl.setRoot('CategoriasPage');
+    this.auth.authenticate(this.creds)
+      .subscribe(response => {
+        this.auth.successfulLogin(response.headers.get('Authorization'));
+        this.navCtrl.setRoot('CategoriasPage');
     },
     error => {});
   }
